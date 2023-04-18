3231281adb7f5486cae68d77f569da79a9850519
marlisajp <marlisajp@gmail.com>
2023-04-18 16:01:53 -0400

    added search functionality

--- client/components/AllCoffee/AllCoffee.js
+++ client/components/AllCoffee/AllCoffee.js
@@ -5,8 +5,9 @@ import { allCoffeeSelector } from '../../redux/reducers/allCoffeeReducer';
 
 const AllCoffee = () => {
   const coffees = useSelector(allCoffeeSelector);
-  const [selectedOption, setSelectedOption] = useState('default');
   const dispatch = useDispatch();
+  const [selectedOption, setSelectedOption] = useState('default');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     dispatch(fetchAllCoffeeAsync());
@@ -18,23 +19,33 @@ const AllCoffee = () => {
     if (selectedOption === 'name') return a.name.localeCompare(b.name);
   });
 
+  const filteredCoffee = sortedCoffee.filter((coffee) =>
+    coffee.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   const handleSort = (event) => {
     setSelectedOption(event.target.value);
   };
 
   return (
     <>
-      <div className='sort'>
-        <span>Sort By: </span>
-        <select value={selectedOption} onChange={handleSort}>
-          <option value='default'>Default</option>
-          <option value='price'>Price</option>
-          <option value='name'>Name</option>
-        </select>
+      <div className='sort-search-container'>
+        <div className='sort'>
+          <span>Sort By: </span>
+          <select value={selectedOption} onChange={handleSort}>
+            <option value='default'>Default</option>
+            <option value='price'>Price</option>
+            <option value='name'>Name</option>
+          </select>
+        </div>
+        <div className='search'>
+          <span>Search for Coffee: </span>
+          <input value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
       </div>
       <div className='container'>
         {coffees ? (
-          sortedCoffee.map((coffee) => (
+          filteredCoffee.map((coffee) => (
             <div key={coffee.id} className='coffee-container'>
               <img className='coffee-img' src={coffee.imageUrl} />
               <p>{coffee.name}</p>
