fd8c26d213d35aec9c0d44e48a3453a1018b4dcd
marlisajp <marlisajp@gmail.com>
2023-04-26 12:11:27 -0400

    deployment stuff

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
 console.log('Opening database connection');
 
-const db = new Sequelize('postgres://localhost:5432/coffeeshop', {
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/coffeeshop',
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
