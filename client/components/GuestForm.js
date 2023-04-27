1b236694fc7d8b18726e94368542fa85b68d20c6
natis231@hotmail.com <101681383+Natis23@users.noreply.github.com>
2023-04-26 23:46:40 -0400

    Form

--- client/components/GuestForm.js
+++ client/components/GuestForm.js
@@ -1,20 +1,27 @@
-import { Action } from 'history';
-import React, { useEffect, useState } from 'react';
-import jwt_decode from 'jwt-decode';
-import { fetchUser } from '../redux/store/auth';
-import { useDispatch } from 'react-redux';
+import { Action } from "history";
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import { fetchUser } from "../redux/store/auth";
+import { useDispatch } from "react-redux";
 
 const GuestForm = (props) => {
-  const products = JSON.parse(localStorage.getItem('cartProducts'));
-  const [userInfo, setUserInfo] = useState({});
+  const products = JSON.parse(localStorage.getItem("cartProducts"));
   const [userId, setUserId] = useState(null);
   const [isTokenChecked, setIsTokenChecked] = useState(false);
   const dispatch = useDispatch();
-  console.log(products);
+  const [formState, setFormState] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    address: "",
+    state: "",
+    city: "",
+    zip: "",
+  });
 
   useEffect(() => {
     const checkToken = () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (token) {
         const decodedToken = jwt_decode(token);
         setUserId(decodedToken.id);
@@ -31,7 +38,7 @@ const GuestForm = (props) => {
     if (!isTokenChecked) {
       return;
     }
-    console.log(userId, 'userId');
+    console.log(userId, "userId");
     const fetchUserData = async () => {
       if (userId) {
         const user = await dispatch(fetchUser(userId));
@@ -41,101 +48,106 @@ const GuestForm = (props) => {
     fetchUserData();
   }, [userId, isTokenChecked]);
 
+
+
+
   const pay = async (form) => {
     form.preventDefault();
-    const data = await fetch('http://localhost:3000/api/cart/stripe', {
-      method: 'POST',
+    const data = await fetch("http://localhost:3000/api/cart/stripe", {
+      method: "POST",
       headers: {
-        Accept: 'Application/json',
-        'Content-type': 'Application/json',
+        Accept: "Application/json",
+        "Content-type": "Application/json",
       },
       body: JSON.stringify(products),
     }).then(async (data) => data.json());
     window.location.href = data.url;
   };
 
-  const handleChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserInfo({ ...userInfo, [name]: value });
-  };
+    setFormState({ ...formState, [name]: value });
+  }
 
-  const clearCart = () => localStorage.removeItem('cartProducts');
+  const clearCart = () => localStorage.removeItem("cartProducts");
 
   return (
     <div>
-      <form className='guest'
+      <form className="guest"
         onSubmit={(e) => {
           pay(e);
           clearCart();
         }}
-        method='POST'>
+        method="POST"
+      >
         <div>
-          <label htmlFor='firstname'>
+          <label htmlFor="firstname">
             <small>First Name</small>
           </label>
           <input
-            name='firstname'
-            type='text'
-            value={userInfo.firstName || ''}
-            onChange={handleChange}
+            name="firstname"
+            type="text"
+            value={formState.firstname} 
+            onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor='lastname'>
+          <label htmlFor="lastname">
             <small>Last Name</small>
           </label>
           <input
-            name='lastname'
-            type='text'
-            value={userInfo.lastName || ''}
-            onChange={handleChange}
+            name="lastname"
+            type="text"
+            value={formState.lastname} 
+            onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor='email'>
+          <label htmlFor="email">
             <small>Email</small>
           </label>
           <input
-            name='email'
-            type='text'
-            value={userInfo.email || ''}
-            onChange={handleChange}
+            name="email"
+            type="text"
+            value={formState.email} 
+            onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor='address'>
+          <label htmlFor="address">
             <small>Address</small>
           </label>
           <input
-            name='address'
-            type='text'
-            value={userInfo.address || ''}
-            onChange={handleChange}
+            name="address"
+            type="text"
+            value={formState.address} 
+            onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor='city'>
+          <label htmlFor="city">
             <small>City</small>
           </label>
           <input
-            name='city'
-            type='text'
-            value={userInfo.city || ''}
-            onChange={handleChange}
+            name="city"
+            type="text"
+            value={formState.city} 
+            onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor='state'>
+          <label htmlFor="state">
             <small>State</small>
           </label>
           <select
-            name='state'
-            value={userInfo.state || ''}
-            onChange={handleChange}>
-            <option value='' disabled>
+            name="state"
+            value={formState.state} 
+            onChange={handleInputChange}
+          >
+            <option value="" disabled>
               Select a State
             </option>
             {states.map((state) => (
@@ -146,73 +158,73 @@ const GuestForm = (props) => {
           </select>
         </div>
         <div>
-          <label htmlFor='zip'>
+          <label htmlFor="zip">
             <small>Zipcode</small>
           </label>
           <input
-            name='zip'
-            type='number'
-            value={userInfo.zip || ''}
-            onChange={handleChange}
+            name="zip"
+            type="number"
+            value={formState.zip} 
+            onChange={handleInputChange}
           />
         </div>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
 
 const states = [
-  'Alabama',
-  'Alaska',
-  'Arizona',
-  'Arkansas',
-  'California',
-  'Colorado',
-  'Connecticut',
-  'Delaware',
-  'Florida',
-  'Georgia',
-  'Hawaii',
-  'Idaho',
-  'Illinois',
-  'Indiana',
-  'Iowa',
-  'Kansas',
-  'Kentucky',
-  'Louisiana',
-  'Maine',
-  'Maryland',
-  'Massachusetts',
-  'Michigan',
-  'Minnesota',
-  'Mississippi',
-  'Missouri',
-  'Montana',
-  'Nebraska',
-  'Nevada',
-  'New Hampshire',
-  'New Jersey',
-  'New Mexico',
-  'New York',
-  'North Carolina',
-  'North Dakota',
-  'Ohio',
-  'Oklahoma',
-  'Oregon',
-  'Pennsylvania',
-  'Rhode Island',
-  'South Carolina',
-  'South Dakota',
-  'Tennessee',
-  'Texas',
-  'Utah',
-  'Vermont',
-  'Virginia',
-  'Washington',
-  'West Virginia',
-  'Wisconsin',
-  'Wyoming',
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
 ];
 
 export default GuestForm;
